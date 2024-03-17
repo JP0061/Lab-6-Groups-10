25e33f3772d80aefb41c17270df7c24c7cae3680
John <jcooper6@ufl.edu>
2024-03-17 12:50:57 -0400

    This is my first addition to the repo

---
+++ main.py
@@ -0,0 +1,40 @@
+def encode(decoded_password):
+    # Encodes the password adding 3 to each digit.
+    # Uses modulo operator to make sure the result is from 0-9
+    encoded_password = ""
+    for digit in decoded_password:
+        encoded_digit = (int(digit) + 3) % 10
+        encoded_password += str(encoded_digit)
+    return encoded_password
+
+
+def decode(encoded_password):
+    pass
+
+
+def main():
+    while True:
+        print('Menu\n'
+              '-------------\n'
+              '1. Encode\n'
+              '2. Decode\n'
+              '3. Quit\n')
+
+        menu_selection = int(input('Please enter an option: '))
+
+        if menu_selection == 1:
+            passcode = input('Please enter your password to encode: ')
+            encoded_password = encode(passcode)
+            print('Your password has been encoded and stored!\n')
+
+        if menu_selection == 2:
+            decoded_password = decode(encoded_password)
+            print(f'The encoded password is {encoded_password}, and the original password is {decoded_password}.\n')
+
+        if menu_selection == 3:
+            quit()
+
+
+if __name__ == "__main__":
+    main()
+
